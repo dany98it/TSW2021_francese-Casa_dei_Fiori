eabc55d58501250fada11b56818be14c7797d3e2
Everysimo <simone.spera.13@gmail.com>
2021-04-24 20:44:33 +0200

    carrello 0.2

--- casadeifiori/src/main/java/model/Carrello.java
+++ casadeifiori/src/main/java/model/Carrello.java
@@ -1,14 +1,66 @@
 package model;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+
 public class Carrello {
-	private ArrayList<Item> itemsOrdinati;
+	private ArrayList<ItemCarrello> itemsOrdinati;
 
 	public Carrello() {
 		super();
-		this.itemsOrdinati = new ArrayList<Item>();
+		this.itemsOrdinati = new ArrayList<ItemCarrello>();
+	}
+
+	public ArrayList<ItemCarrello> getItemsOrdinati() {
+		return itemsOrdinati;
 	}
 	
 	
+	public synchronized void addItem(int itemID) {
+	    ItemCarrello order;
+	    for(int i=0; i<itemsOrdinati.size(); i++) {
+	      order = (ItemCarrello )itemsOrdinati.get(i);
+	      if (order.getItem().getId()==itemID) {
+	        order.incrementaQuantita();
+	        return;
+	      }
+	    }
+	    ItemDAO iDao = new  ItemDAO();
+	    
+	    ItemCarrello newOrder;
+		try {
+			newOrder = new ItemCarrello(iDao.doRetrieveByKey(itemID),1);
+			itemsOrdinati.add(newOrder);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  }
+	
+	public synchronized void setNumOrdered(int itemID,
+            int numOrdered) {
+			ItemCarrello order;
+				for(int i=0; i<itemsOrdinati.size(); i++) {
+						order = (ItemCarrello)itemsOrdinati.get(i);
+							if (order.getItem().getId()==itemID) {
+								if (numOrdered <= 0) {
+									itemsOrdinati.remove(i);
+								} else {
+									order.setQuantita(numOrdered);
+								}
+								return;
+							}
+				}
+				ItemDAO iDao = new  ItemDAO();
+				
+				ItemCarrello newOrder = null;
+				try {
+					newOrder = new ItemCarrello(iDao.doRetrieveByKey(itemID),1);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				itemsOrdinati.add(newOrder);
+	}	
 }
