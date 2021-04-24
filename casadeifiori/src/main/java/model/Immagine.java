817efa8aba49c7fce6ac296b652a03c4e9cbacc8
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-04-24 17:19:49 +0200

    Update Immagine.java

--- casadeifiori/src/main/java/model/Immagine.java
+++ casadeifiori/src/main/java/model/Immagine.java
@@ -4,4 +4,41 @@ public class Immagine {
 	private int id;
 	private String url;
 	private String descrizione;
+	
+	
+	public Immagine() {
+		super();
+	}
+
+	public Immagine(int id, String url, String descrizione) {
+		super();
+		this.id = id;
+		this.url = url;
+		this.descrizione = descrizione;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
 }
