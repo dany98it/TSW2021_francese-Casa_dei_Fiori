5c2a22ba87289c8bdaab2ea76a8c3e00310d4a49
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-04-24 17:40:39 +0200

    Update DaoInterfacce.java

--- casadeifiori/src/main/java/model/DaoInterfacce.java
+++ casadeifiori/src/main/java/model/DaoInterfacce.java
@@ -3,12 +3,12 @@ package model;
 import java.sql.SQLException;
 import java.util.Collection;
 
-public interface DaoInterfacce<T> {
+public interface DaoInterfacce<T,E> {
 	public void doSave(T t) throws SQLException;
 
-	public boolean doDelete(int code) throws SQLException;
+	public boolean doDelete(E code) throws SQLException;
 
-	public T doRetrieveByKey(int code) throws SQLException;
+	public T doRetrieveByKey(E code) throws SQLException;
 	
-	public Collection<T> doRetrieveAll(String order) throws SQLException;
+	public Collection<T> doRetrieveAll() throws SQLException;
 }
