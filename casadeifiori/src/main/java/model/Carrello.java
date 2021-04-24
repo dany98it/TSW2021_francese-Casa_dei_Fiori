8ed92b72b81a3ccabc0475447394b12f26719dc0
Everysimo <simone.spera.13@gmail.com>
2021-04-24 19:56:59 +0200

    added carrello 0.1

---
+++ casadeifiori/src/main/java/model/Carrello.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Carrello {
+	private ArrayList<Item> itemsOrdinati;
+
+	public Carrello() {
+		super();
+		this.itemsOrdinati = new ArrayList<Item>();
+	}
+	
+	
+}
