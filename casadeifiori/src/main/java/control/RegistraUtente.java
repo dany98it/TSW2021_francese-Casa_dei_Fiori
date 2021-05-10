1c2bd618b58fcb8bc7ac851bbe0347a2ef54dd18
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-05-10 17:36:21 +0200

    create RegistraUtente

---
+++ casadeifiori/src/main/java/control/RegistraUtente.java
@@ -0,0 +1,64 @@
+package control;
+
+import java.io.IOException;
+/*import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;*/
+import java.sql.Date;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Permessi;
+import model.User;
+import model.UserDAO;
+
+/**
+ * Servlet implementation class RegistraUtente
+ */
+@WebServlet("/RegistraUtente")
+public class RegistraUtente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public RegistraUtente() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = new User();
+		user.setEmail(request.getParameter("email"));
+		user.setPassword(request.getParameter("password"));
+		user.setNome(request.getParameter("nome"));
+		user.setCognome(request.getParameter("cognome"));
+		user.setdataNascita(Date.valueOf(request.getParameter("dataNascita")));
+		user.setPermessi(Permessi.user);
+		user.setTelefono(request.getParameter("telefono"));
+		UserDAO userdao=new UserDAO();
+		try {
+			userdao.doSave(user);
+		} catch (SQLException e) {
+			response.sendRedirect("singInPage.jsp");
+		}
+		response.sendRedirect("loginPage.jsp");
+	}
+
+}
