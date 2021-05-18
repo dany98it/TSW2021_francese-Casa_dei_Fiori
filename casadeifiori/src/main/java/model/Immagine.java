359beb845c5dfec0b8816d8373c481d901957087
dany98sa <56975152+dany98sa@users.noreply.github.com>
2021-05-18 22:06:02 +0200

    modificato il metodo di salvataggio delle immagine

--- casadeifiori/src/main/java/model/Immagine.java
+++ casadeifiori/src/main/java/model/Immagine.java
@@ -1,14 +1,18 @@
 package model;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.Base64;
+import java.util.Map;
 
 import javax.sql.rowset.serial.SerialBlob;
 import javax.sql.rowset.serial.SerialException;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
 public class Immagine {
 	private int id;
@@ -50,21 +54,13 @@ public class Immagine {
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
-	public String toBase64(Blob img) throws Exception {
-		InputStream in=img.getBinaryStream();
-		ByteArrayOutputStream out= new ByteArrayOutputStream();
-		byte[] buffer=new byte[4096];
-		int bytesRead=-1;
-		while ((bytesRead = in.read(buffer)) != -1) {
-			out.write(buffer, 0, bytesRead);                  
-        }
-		byte[] imageBytes = out.toByteArray();
-		in.close();
-		out.close();
-		return Base64.getEncoder().encodeToString(imageBytes);
-	}
-	public Blob toBlob(String img) throws Exception {
-		byte[] imageBytes = Base64.getDecoder().decode(img);
-		return new SerialBlob(imageBytes);
+	public Object caricaImmagine(File f) throws Exception {
+		Map config = ObjectUtils.asMap(
+				  "cloud_name", "hnqb5wfvq",
+				  "api_key", "876962418587798",
+				  "api_secret", "leFJnkkTvShSQLQTME2AoXsciRY");
+		Cloudinary cloudinary = new Cloudinary(config);
+		Map result=cloudinary.uploader().upload(f, ObjectUtils.emptyMap());
+		return result.get("url");
 	}
 }
